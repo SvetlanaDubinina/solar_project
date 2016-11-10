5ac8660bc1c0a76baa47688a089a9e7230150e99
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-11-10 17:29:16 +0300

    Sdelano

--- solar_model.py
+++ solar_model.py
@@ -7,9 +7,7 @@ gravitational_constant = 6.67408E-11
 
 def calculate_force(body, space_objects):
     """Вычисляет силу, действующую на тело.
-
     Параметры:
-
     **body** — тело, для которого нужно вычислить дейстующую силу.
     **space_objects** — список объектов, которые воздействуют на тело.
     """
@@ -18,33 +16,37 @@ def calculate_force(body, space_objects):
     for obj in space_objects:
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
+        if body == obj:
+            continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        body.Fx += gravitational_constant*body.m*obj.m*(-body.x + obj.x)/(r**3)
+        body.Fy += gravitational_constant*body.m*obj.m*(-body.y + obj.y)/(r**3)
 
 
 def move_space_object(body, dt):
     """Перемещает тело в соответствии с действующей на него силой.
+     Параметры:
 
-    Параметры:
-
-    **body** — тело, которое нужно переместить.
-    """
+     **body** — тело, которое нужно переместить.
+     """
 
     ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
+    body.x += (1/2)*ax*(dt**2)+ body.Vx*dt
     body.Vx += ax*dt
     # FIXME: not done recalculation of y coordinate!
+
+    ay = body.Fy/body.m
+    body.y += (1/2)*ay*(dt**2) + body.Vy*dt
+    body.Vy += ay*dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
     """Пересчитывает координаты объектов.
+     Параметры:
 
-    Параметры:
-
-    **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
-    **dt** — шаг по времени
-    """
+     **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
+     **dt** — шаг по времени
+     """
 
     for body in space_objects:
         calculate_force(body, space_objects)
